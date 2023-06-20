9e1ead060f932c76833dd1c84b0c5f9732f95d1f
Trevor van Hoof <trevor@applied.co>
2023-06-20 14:26:45 +0200

    Cleaner windows.h

---
+++ windont.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef VC_EXTRALEAN 
+#define VC_EXTRALEAN 1
+#endif
+#ifndef WIN32_LEAN_AND_MEAN 
+#define WIN32_LEAN_AND_MEAN 1
+#endif
+#include <Windows.h>
+#include <urlmon.h>
+#undef near
+#undef far
+#undef min
+#undef max
+#undef assert
